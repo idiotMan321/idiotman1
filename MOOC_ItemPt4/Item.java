7b8e9c997ca00f6c5d69b9b2b3ef07809649353e
leClandestin <128958974+leClandestin@users.noreply.github.com>
2025-06-21 14:16:21 -0400

    Update Item.java
    this code contains code that can display current time

--- MOOC_ItemPt4/Item.java
+++ MOOC_ItemPt4/Item.java
@@ -2,16 +2,16 @@ package MOOC;
 
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatter; //display current time
 
 public class Item {
 
     private String name;
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt;//display current time
 
     public Item(String name) {
         this.name = name;
-        this.createdAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now();//display current time
     }
 
     public String getName() {
@@ -20,7 +20,7 @@ public class Item {
 
     @Override
     public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"); //display current time
 
         return this.name + " (created at: " + formatter.format(this.createdAt) + ")";
     }
